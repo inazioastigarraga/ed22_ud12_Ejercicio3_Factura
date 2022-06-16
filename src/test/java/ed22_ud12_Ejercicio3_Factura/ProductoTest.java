c1e2cd41d04cdf8ab927a6821b235b3e55d011b3
Inazio <inazio.astigarraga@gmail.com>
2022-06-17 00:13:33 +0200

    Ej3

--- src/test/java/ed22_ud12_Ejercicio3_Factura/ProductoTest.java
+++ src/test/java/ed22_ud12_Ejercicio3_Factura/ProductoTest.java
@@ -1,53 +1,43 @@
 package ed22_ud12_Ejercicio3_Factura;
 
+import ed22_ud12_Ejercicio3_Factura.Producto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-
 class ProductoTest {
-	
-	Producto p1;
-	Producto p2;
-	Producto p3;
-
-	@BeforeEach
-	void setUp() throws Exception {
-		this.p1 = new Producto("pan",0.50f, 1);
-		this.p2 = null;
-		this.p3 = p1;
-	}
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-	
-	@RepeatedTest(1000)
-	void testIfNull() {
-		assertNull(p1);
-	}
-	@RepeatedTest(1000)
-	void testIfIsNotNull() {
-		assertNotNull(p1);
-	}
-	@Test()
-	void testSameObject() {
-		assertSame(p1, p3);
-	}
-	@Test()
-	void testNotSameObject() {
-		assertNotSame(p1, p3);
-	}
-	@Test()
-	void testIfIsTrue() {
-		p1.setNombre("pan");
-		assertTrue(p1.getNombre() == "pan");
-	}
-	@Test()
-	void testIfIsFalse() {
-		p1.setNombre("pepino");
-		assertFalse(p1.getNombre() != "pan");
-	}	
-
+    private Producto p1;
+    @BeforeEach
+    public void initEach(){
+        p1 = new Producto("lechuga",1.40f, 5);
+    }
+    
+    
+    @Test
+    @DisplayName("Calcula precio total")
+    void precioTotal() {
+        assertEquals(1.40f,p1.precioTotal());
+    }
+    
+    
+    @Test
+    @DisplayName("Obtiene nombre producto")
+    void getNombre(){
+        assertEquals("lechuga",p1.getNombre());
+    }
+    
+    
+    @Test
+    @DisplayName("Obtiene cantidad producto")
+    void getPrecio(){
+        assertEquals(1.40f,p1.getPrecio());
+    }
+    
+    
+    @Test
+    @DisplayName("Obtiene cantidad producto")
+    void getCantidad(){
+        assertEquals(5,p1.getCantidad());
+    }
 }
